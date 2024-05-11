d0e3b427a57ba42c6ad495dde88b22c5ec529bac
Anthony Barry <ajbarry99@gmail.com>
2024-05-11 13:31:01 +1000

    cleanup.

--- chrome/src/devtools.ts
+++
@@ -1,9 +0,0 @@
-// chrome.devtools.panels.sidePanels.create(
-//   'My Side Panel',
-//   'icon.png',
-//   'dist/chrome-devtools/index.html',
-//   function(panel) {
-//     console.log('Panel created', panel);
-//     // code to execute when the panel is displayed goes here
-//   }
-// );
