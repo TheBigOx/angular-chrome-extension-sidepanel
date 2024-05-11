8eabc9296f1b84809c014096a2e8b2cfdedd7630
Anthony Barry <ajbarry99@gmail.com>
2024-05-11 13:14:51 +1000

    Initial commit.

---
+++ chrome/src/devtools.ts
@@ -0,0 +1,9 @@
+// chrome.devtools.panels.sidePanels.create(
+//   'My Side Panel',
+//   'icon.png',
+//   'dist/chrome-devtools/index.html',
+//   function(panel) {
+//     console.log('Panel created', panel);
+//     // code to execute when the panel is displayed goes here
+//   }
+// );
