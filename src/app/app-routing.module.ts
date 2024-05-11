e3daad8fe54d23eaaf05f2dfd60c3c467677f6d3
Anthony Barry <ajbarry99@gmail.com>
2024-05-12 05:39:50 +1000

    added testing pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) }, 
-  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'help', loadChildren: () => import('./pages/help/help.module').then(m => m.HelpModule) },
+  { path: 'chat', loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatModule) },
+  { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) },
+  { path: 'liked', loadChildren: () => import('./pages/liked/liked.module').then(m => m.LikedModule) }
 ];
 
 @NgModule({
