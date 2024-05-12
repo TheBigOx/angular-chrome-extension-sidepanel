0b79720e4b869bbdba492693622b8496280745cd
Anthony Barry <ajbarry99@gmail.com>
2024-05-13 02:45:47 +1000

    options page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chrome-sidepanel';
+  private router = inject(Router);
+  showNavMenu = false;
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      console.log(event.urlAfterRedirects); // logs the current path
+      if(event.urlAfterRedirects === '/options'){
+        this.showNavMenu = false;
+      } else {
+        this.showNavMenu = true;
+      }
+    });
+  }
 }
